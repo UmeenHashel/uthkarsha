c56cccb31922e3e5db9b55143d736396f862d18c
Pathum Lakshitha <96337912+PathumLakshitha@users.noreply.github.com>
2024-06-22 19:26:56 +0530

    update homepage

--- index.php
+++ index.php
@@ -24,24 +24,23 @@
       <div class="row">
         <div class="column about">
           <h2>About Us</h2>
-          <p>Welcome to UTHKARSHA CLOTHING STORE! As a leading fashion retailer, we offer high-quality apparel and exceptional service. Our mission is to make fashion accessible to everyone, with a wide range of styles to suit all tastes. We believe in fashion's power to express individuality and confidence, curating collections that blend the latest trends with timeless classics. Thank you for choosing us as your fashion destination. We look forward to serving you!</p>
+          <p>Welcome to UTHKARSHA CLOTHING STORE! As a leading fashion retailer, we offer high-quality apparel and exceptional service. Our mission is to make fashion accessible to everyone, with a wide range of styles to suit all tastes. Thank you for choosing us as your fashion destination. We look forward to serving you!</p>
         </div>
         <div class="column links">
-          <h2>Quick Links</h2>
+          <h2>Account</h2>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Shop</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Cart</a></li>
             <li><a href="#">Blog</a></li>
             <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
         <div class="column connect">
-          <h2>Connect with Us</h2>
+          <h2>INFORMATION</h2>
           <ul>
-            <li><a href="#">Instagram</a></li>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Pinterest</a></li>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">Privary Policy</a></li>
           </ul>
         </div>
       </div>
